c88f0364f116616500c0e1bd63dd1419d45d4f6f
lucasalmeida01 <lucas.almeida1705@outlook.com>
2022-02-15 18:43:05 -0300

    First method creating a new user with username validation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
+//middleware
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
+// DONE
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.some(
+    (user) => user.username === username
+  );
+
+  if (userAlreadyExists) {
+    return response.status(400).json({
+      error: "User already exists!"
+    });
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.status(201).json(user);
+
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
@@ -38,4 +63,5 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
+app.listen('3333');
 module.exports = app;
