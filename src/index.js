f806f670e6614c01ed537fc6819f1e49d2c7a155
lucasalmeida01 <lucas.almeida1705@outlook.com>
2022-02-16 12:12:55 -0300

    Last changins

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find((user) => user.username === username);
 
   if (!user) {
-    return response.status(400).json({
+    return response.status(404).json({
       error: "User not found!"
     });
   }
@@ -58,8 +58,8 @@ app.post('/users', (request, response) => {
 //DONE
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
-  return response.status(201).json(user.todos);
-  // DONE
+  return response.json(user.todos);
+
 });
 
 //DONE
@@ -70,7 +70,7 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   const todoOperation = {
     id: uuidv4(),
     title,
-    done: "false",
+    done: false,
     deadline: new Date(deadline),
     created_at: new Date()
   }
@@ -99,9 +99,9 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 
   else {
     user.todos[indexTodoExists].title = title;
-    user.todos[indexTodoExists].deadline = deadline;
+    user.todos[indexTodoExists].deadline = new Date(deadline);
 
-    return response.status(201).send();
+    return response.json(todoExists);
   }
 
 });
@@ -121,20 +121,20 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   }
 
   else {
-    user.todos[indexTodoExists].done = "true";
+    user.todos[indexTodoExists].done = true;
 
-    return response.status(201).send();
+    return response.json(todoExists);
   }
 });
 
+//DONE
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { id } = request.params;
 
-  const todoExists = user.todos.find(todo => todo.id === id);
   const indexTodoExists = user.todos.findIndex(todo => todo.id === id);
 
-  if (!todoExists) {
+  if (indexTodoExists === -1) {
     return response.status(404).json({
       error: "To do not found for this user!"
     });
@@ -143,7 +143,7 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   else {
     user.todos.splice(indexTodoExists, 1);
 
-    return response.status(201).send();
+    return response.status(204).send();
   }
 });
 
